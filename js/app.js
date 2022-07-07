cfa243d7977b191edb8e9d6e280eafd90c10e659
CIFU1199 <edwkamilo@gmail.com>
2022-07-07 16:10:27 -0500

    Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -10,16 +10,34 @@ function eventListeners(){
 }
 
 //classes
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI{
+
+}
+//Instanciar 
+const ui = new UI();
+
+let presupuesto;
 
 //Funciones 
 
 function preguntarPresupuesto(){
     const presupuestoUser = prompt('¿Cual es tu presupuesto?');
 
-    console.log(Number(presupuestoUser));
+    //console.log(Number(presupuestoUser));
 
-    if (presupuestoUser ==='' || presupuestoUser === null || isNaN(presupuestoUser)){
+    if (presupuestoUser ==='' || presupuestoUser === null || isNaN(presupuestoUser) || presupuestoUser <= 0){
         window.location.reload();
     }
+
+    //Presupuesto valido 
+    presupuesto = new Presupuesto(presupuestoUser);
+    console.log(presupuesto);
 }
